75034e2246556c37716ffc9ae4a9fc2f0d44edc8
Tomas Jacik <tomas@jacik.cz>
2021-06-02 03:49:31 +0200

    Update nette/coding-standard to 3.1

--- config/sunfox.php
+++ config/sunfox.php
@@ -1,11 +1,15 @@
 <?php declare(strict_types=1);
 
+use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NullableTypeDeclarationForDefaultNullValueFixer;
+use PhpCsFixer\Fixer\FunctionNotation\VoidReturnFixer;
+use PhpCsFixer\Fixer\ListNotation\ListSyntaxFixer;
+use PhpCsFixer\Fixer\Whitespace\CompactNullableTypehintFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../symplify/easy-coding-standard/config/set/psr12.php');
-    $containerConfigurator->import(__DIR__ . '/../../../symplify/easy-coding-standard/config/set/php71.php');
     $containerConfigurator->import(__DIR__ . '/../../../nette/coding-standard/preset/php71.php');
 
     $parameters = $containerConfigurator->parameters();
@@ -19,10 +23,24 @@ return function (ContainerConfigurator $containerConfigurator): void {
         PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\ControlStructureSpacingSniff::class . 'SpacingBeforeClose' => null,
         PhpCsFixer\Fixer\Operator\NewWithBracesFixer::class => null,
         PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer::class => null,
+        PhpCsFixer\Fixer\PhpTag\LinebreakAfterOpeningTagFixer::class => null,
         SlevomatCodingStandard\Sniffs\Commenting\RequireOneLinePropertyDocCommentSniff::class => null,
     ]);
 
     $services = $containerConfigurator->services();
+
+    // PHP 7.1
+    $services->set(VisibilityRequiredFixer::class)
+        ->call('configure', [[
+            'elements' => ['const', 'property', 'method'],
+        ]]);
+    $services->set(ListSyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+    $services->set(NullableTypeDeclarationForDefaultNullValueFixer::class);
+    $services->set(CompactNullableTypehintFixer::class);
+    $services->set(VoidReturnFixer::class);
 
     // Detect dead code
     $services->set(SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class);
